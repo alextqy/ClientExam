d627304580f08a54f9cfbe2d7826252715453dd2
alextqy <289959263@qq.com>
2022-10-24 16:28:27 +0800

    new

---
+++ client-exam/client-exam/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace client_exam;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ client-exam/client-exam/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace client_exam;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
