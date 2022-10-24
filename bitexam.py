b13b178df337e4f7f3d17bf1ea96e9706fa64632
alextqy <289959263@qq.com>
2022-10-24 15:54:23 +0800

    init-maui

--- bitexam.py
+++
@@ -1,9 +0,0 @@
-# -*- coding:utf-8 -*-
-from Public.Common import *
-from Template.MainTemplate import *
-
-if __name__ == '__main__':
-    App = QApplication(argv)
-    MainObject = MainTemplate()
-    MainObject.show()
-    exit(App.exec())
