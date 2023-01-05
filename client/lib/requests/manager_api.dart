c8994bfde515dbcaaafe741ebe4b95c5a393d7f4
alextqy <289959263@qq.com>
2023-01-05 17:11:26 +0800

    coding

--- client/lib/requests/manager_api.dart
+++ client/lib/requests/manager_api.dart
@@ -47,11 +47,11 @@ class ManagerApi extends ResponseHelper {
     return BaseModel.fromJson(jsonDecode(response.body));
   }
 
-  Future<BaseModel> newManager([
+  Future<BaseModel> newManager({
     String account = '',
     String password = '',
     String name = '',
-  ]) async {
+  }) async {
     var response = await http.post(
       Uri.http(url, '/New/Manager'),
       body: {
@@ -66,9 +66,9 @@ class ManagerApi extends ResponseHelper {
     return BaseModel.fromJson(jsonDecode(response.body));
   }
 
-  Future<BaseModel> managerDisabled([
+  Future<BaseModel> managerDisabled({
     int id = 0,
-  ]) async {
+  }) async {
     var response = await http.post(
       Uri.http(url, '/Manager/Disabled'),
       body: {
@@ -81,10 +81,10 @@ class ManagerApi extends ResponseHelper {
     return BaseModel.fromJson(jsonDecode(response.body));
   }
 
-  Future<BaseModel> managerChangePassword([
+  Future<BaseModel> managerChangePassword({
     String newPassword = '',
     int id = 0,
-  ]) async {
+  }) async {
     var response = await http.post(
       Uri.http(url, '/Manager/Change/Password'),
       body: {
@@ -98,11 +98,11 @@ class ManagerApi extends ResponseHelper {
     return BaseModel.fromJson(jsonDecode(response.body));
   }
 
-  Future<BaseModel> updateManagerInfo([
+  Future<BaseModel> updateManagerInfo({
     String name = '',
     int permission = 0,
     int id = 0,
-  ]) async {
+  }) async {
     var response = await http.post(
       Uri.http(url, '/Update/Manager/Info'),
       body: {
@@ -117,13 +117,13 @@ class ManagerApi extends ResponseHelper {
     return BaseModel.fromJson(jsonDecode(response.body));
   }
 
-  Future<BaseModel> managerList([
+  Future<BaseModel> managerList({
     int page = 1,
     int pageSize = 10,
     String stext = '',
     bool state = false,
     int permission = 0,
-  ]) async {
+  }) async {
     var response = await http.post(
       Uri.http(url, '/Manager/List'),
       body: {
@@ -140,9 +140,9 @@ class ManagerApi extends ResponseHelper {
     return BaseModel.fromJson(jsonDecode(response.body));
   }
 
-  Future<BaseModel> managerInfo([
+  Future<BaseModel> managerInfo({
     int id = 0,
-  ]) async {
+  }) async {
     var response = await http.post(
       Uri.http(url, '/Manager/Info'),
       body: {
