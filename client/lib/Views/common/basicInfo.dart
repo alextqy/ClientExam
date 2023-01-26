0fa7af65999a5c58f03d401b1160e914bd235f75
alextqy <289959263@qq.com>
2023-01-26 18:27:19 +0800

    update

--- client/lib/Views/common/basicInfo.dart
+++ client/lib/Views/common/basicInfo.dart
@@ -1,6 +1,6 @@
 import 'package:client/public/lang.dart';
 
-List<int> perPageDropList = <int>[5, 10, 50, 100];
+List<int> perPageDropList = <int>[10, 50, 100];
 List<String> stateDropList = <String>[
   Lang().all,
   Lang().normal,
