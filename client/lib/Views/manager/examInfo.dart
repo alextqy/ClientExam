3e895d732b10da6a5e8eede1081424669c2bd9fe
alextqy <289959263@qq.com>
2023-02-07 17:43:33 +0800

    coding

---
+++ client/lib/Views/manager/examInfo.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: file_names
+
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:client/public/lang.dart';
+import 'package:client/public/tools.dart';
+import 'package:client/Views/common/basic_info.dart';
+import 'package:client/Views/common/toast.dart';
+import 'package:client/Views/common/menu.dart';
