a503eb62ab80b5beac39e1bfb286fbf72ec40e80
alextqy <289959263@qq.com>
2023-02-14 11:07:17 +0800

    coding

--- client/lib/Views/manager/examInfo.dart
+++ client/lib/Views/manager/examInfo.dart
@@ -130,6 +130,18 @@ class ExamInfoState extends State<ExamInfo> {
     return List<DataRow>.generate(
       examInfoNotifier.examInfoListModel.length,
       (int index) => DataRow(
+        color: MaterialStateProperty.resolveWith<Color?>(
+            (Set<MaterialState> states) {
+          // All rows will have the same selected color.
+          if (states.contains(MaterialState.selected)) {
+            return Theme.of(context).colorScheme.primary.withOpacity(0.2);
+          }
+          // Even rows will have a grey color.
+          // if (index.isEven) {
+          //   return Colors.grey.withOpacity(0.3);
+          // }
+          return null; // Use default value for other states and odd rows.
+        }),
         cells: <DataCell>[
           DataCell(
             Text(
