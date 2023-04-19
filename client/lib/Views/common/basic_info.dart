47007158e4400d6809f0827cd1a95b4fd5c8758e
alextqy <289959263@qq.com>
2023-04-19 14:38:56 +0800

    update

--- client/lib/Views/common/basic_info.dart
+++ client/lib/Views/common/basic_info.dart
@@ -156,3 +156,23 @@ Tooltip questionMark(BuildContext context, String languageMemo) {
     ),
   );
 }
+
+Scrollbar scrollbarWidget(dynamic widgets) {
+  ScrollController controllerOutside = ScrollController();
+  ScrollController controllerInside = ScrollController();
+  return Scrollbar(
+    thumbVisibility: true,
+    radius: const Radius.circular(0),
+    thickness: 10,
+    controller: controllerOutside,
+    child: SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      controller: controllerOutside,
+      child: SingleChildScrollView(
+        controller: controllerInside,
+        scrollDirection: Axis.vertical,
+        child: widgets,
+      ),
+    ),
+  );
+}
